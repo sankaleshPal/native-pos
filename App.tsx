2803f4a40913d17de949be37ff437bc6d767e6e5
sankalesh <sankalesh.paltribe@gmail.com>
2025-12-25 14:28:30 +0530

    release: v2.1.0 - FCM Implementation and Package Rename

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { JSX, useEffect } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import RootNavigator from './src/navigation/RootNavigator';
 import { QueryProvider } from './src/providers/QueryProvider';
 import { getDatabase } from './src/db/database';
 import { seedLargeMenuData } from './src/db/seedLargeMenu';
 
-import { StatusBar } from 'react-native';
+import { Alert, StatusBar } from 'react-native';
 import { useThemeStore } from './src/store/themeStore';
 
 import { notificationService } from './src/services/NotificationService';
@@ -13,11 +13,12 @@ import GlobalOrderAlert from './src/components/GlobalOrderAlert';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-export default function App(): React.JSX.Element {
+import { PermissionsAndroid } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
+export default function App(): JSX.Element {
   const { theme } = useThemeStore();
 
-  React.useEffect(() => {
+  useEffect(() => {
     // Initialize database and seed data
     const initDB = async () => {
       getDatabase();
@@ -27,6 +28,38 @@ export default function App(): React.JSX.Element {
 
     // Initialize Notifications
     notificationService.initialize();
+    reqPermissionAndroid();
+  }, []);
+
+  const reqPermissionAndroid = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+        {
+          title: 'Notification Permission',
+          message: 'Allow notifications to stay updated',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        Alert.alert('Permission granted');
+        console.log('Permission granted');
+      } else {
+        Alert.alert('Permission denied');
+        console.log('Permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
   }, []);
 
   return (
--- android/app/src/main/java/com/hipalz/demopos/MainActivity.kt
+++ android/app/src/main/java/com/hipalz/demopos/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.test
+package com.hipalz.demopos
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
